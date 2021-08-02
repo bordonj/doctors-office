b24d33f650a4ccda8fe54720db44160506888eaa
Simple Dimple John <simpledimplejohn@gmail.com>
2021-08-02 15:49:17 -0700

    mvp up and running
    Co-authored-by: gurshankaile206 <gurshankaile206@users.noreply.github.com>
    Co-authored-by: bordonj <bordonj@users.noreply.github.com>

--- DoctorsOffice/Controllers/PatientsController.cs
+++ DoctorsOffice/Controllers/PatientsController.cs
@@ -1,75 +1,114 @@
-// using Microsoft.AspNetCore.Mvc.Rendering;
-// using Microsoft.EntityFrameworkCore;
-// using Microsoft.AspNetCore.Mvc;
-// using DoctorsOffice.Models;
-// using System.Collections.Generic;
-// using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using DoctorsOffice.Models;
+using System.Collections.Generic;
+using System.Linq;
 
-// namespace DoctorsOffice.Controllers
-// {
-//   public class PatientsController : Controller
-//   {
-//     private readonly DoctorsOfficeContext _db;
+namespace DoctorsOffice.Controllers
+{
+  public class PatientsController : Controller
+  {
+    private readonly DoctorsOfficeContext _db;
 
-//     public PatientsController(DoctorsOfficeContext db)
-//     {
-//       _db = db;
-//     }
+    public PatientsController(DoctorsOfficeContext db)
+    {
+      _db = db;
+    }
 
-//     public ActionResult Index()
-//     {
-//       List<Patient> model = _db.Patients.Include(patient => patient.Doctor).ToList();
-//       return View(model);
-//     }
+public ActionResult Index()
+{
+    return View(_db.Patients.ToList());
+}
 
-//     public ActionResult Create()
-//     {
-//       ViewBag.DoctorId = new SelectList(_db.Doctors, "DoctorId", "Name");
-//       return View();
-//     }
+    public ActionResult Create()
+    {
+      ViewBag.DoctorId = new SelectList(_db.Doctors, "DoctorId", "Name");
+      return View();
+    }
 
-//     [HttpPost]
-//     public ActionResult Create(Patient patient)
-//     {
-//       _db.Patients.Add(patient);
-//       _db.SaveChanges();
-//       return RedirectToAction("Index");
-//     }
+    [HttpPost]
+    public ActionResult Create(Patient patient, int DoctorId)
+    {
+      _db.Patients.Add(patient);
+      _db.SaveChanges();
+      if (DoctorId != 0)
+      {
+        _db.DoctorPatient.Add(new DoctorPatient() { DoctorId = DoctorId, PatientId = patient.PatientId});
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
-//     public ActionResult Details(int id)
-//     {
-//       Patient thisPatient = _db.Patients.FirstOrDefault(patient => patient.PatientId == id);
-//       return View(thisPatient);
-//     }
+public ActionResult Details(int id)
+{
+    var thisPatient = _db.Patients
+        .Include(patient => patient.JoinEntities)
+        .ThenInclude(join => join.Doctor) // to include the actual Doctor objects themselves
+        // this loads the Doctor of each DoctorPatient 
+        // remember, DoctorPatient is simply a reference to a relationship
+        .FirstOrDefault(patient => patient.PatientId == id);
+    return View(thisPatient);
+}
 
-//     public ActionResult Edit(int id)
-//     {
-//       var thisPatient = _db.Patients.FirstOrDefault(patient => patient.PatientId == id);
-//       ViewBag.DoctorId = new SelectList(_db.Doctors, "DoctorId", "Name");
-//       return View(thisPatient);
-//     }
+    public ActionResult Edit(int id)
+    {
+      var thisPatient = _db.Patients.FirstOrDefault(patient => patient.PatientId == id);
+      ViewBag.DoctorId = new SelectList(_db.Doctors, "DoctorId", "Name");
+      return View(thisPatient);
+    }
 
-//     [HttpPost]
-//     public ActionResult Edit(Patient patient)
-//     {
-//       _db.Entry(patient).State = EntityState.Modified;
-//       _db.SaveChanges();
-//       return RedirectToAction("Index");
-//     }
+    [HttpPost]
+    public ActionResult Edit(Patient patient, int DoctorId)
+    {
+      if (DoctorId != 0)
+      {
+        _db.DoctorPatient.Add(new DoctorPatient() { DoctorId = DoctorId, PatientId = patient.PatientId}); 
+      }
+      _db.Entry(patient).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
-//     public ActionResult Delete(int id)
-//     {
-//       var thisPatient = _db.Patients.FirstOrDefault(patient => patient.PatientId == id);
-//       return View(thisPatient);
-//     }
+    public ActionResult AddDoctor(int id)
+    {
+        var thisPatient = _db.Patients.FirstOrDefault(patient => patient.PatientId == id);
+        ViewBag.DoctorId = new SelectList(_db.Doctors, "DoctorId", "Name");
+        return View(thisPatient);
+    }
 
-//     [HttpPost, ActionName("Delete")]
-//     public ActionResult DeleteConfirmed(int id)
-//     {
-//       var thisPatient = _db.Patients.FirstOrDefault(patient => patient.PatientId == id);
-//       _db.Patients.Remove(thisPatient);
-//       _db.SaveChanges();
-//       return RedirectToAction("Index");
-//     }
-//   }
-// }
+    [HttpPost]
+    public ActionResult AddDoctor(Patient patient, int DoctorId)
+    {
+      if (DoctorId != 0)
+    {
+      _db.DoctorPatient.Add(new DoctorPatient() { DoctorId = DoctorId, PatientId = patient.PatientId });
+    }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+}
+    public ActionResult Delete(int id)
+    {
+      var thisPatient = _db.Patients.FirstOrDefault(patient => patient.PatientId == id);
+      return View(thisPatient);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisPatient = _db.Patients.FirstOrDefault(patient => patient.PatientId == id);
+      _db.Patients.Remove(thisPatient);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public ActionResult DeleteDoctor(int joinId)
+    {
+        var joinEntry = _db.DoctorPatient.FirstOrDefault(entry => entry.DoctorPatientId == joinId);
+        _db.DoctorPatient.Remove(joinEntry);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
+  }
+}
